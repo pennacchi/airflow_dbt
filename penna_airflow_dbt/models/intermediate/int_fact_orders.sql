b3879d54a68e30c0e6923cfd61a917b2ca6dad1f
Pennacchi <rodrigopennacchi@gmail.com>
2025-07-09 21:34:52 -0300

    Adjusting freight value of fact_orders

--- penna_airflow_dbt/models/intermediate/int_fact_orders.sql
+++ penna_airflow_dbt/models/intermediate/int_fact_orders.sql
@@ -1,40 +1,30 @@
-with fact_orders as (
-  select 
+/*
+To create the fact_orders table, we need to join the order_resume table with the order_details table.
+This join would create a replication of our freight column for each order_detail row.
+We decided to create each freight as a product.
+So we first create our order_and_details table and then add the freight as a product.
+*/
+
+with order_and_details as (
+    select 
       o.order_id
     , o.customer_id
     , o.employee_id
     , o.order_date
     , od.product_id
-    , p.product_name
     , od.unit_price
     , od.quantity
-    , od.discount
-    , o.freight
-    , (od.unit_price * od.quantity)                                  as product_sales_revenue
-    , (od.unit_price * od.quantity) + o.freight                      as gross_revenue
-    , (od.unit_price * od.quantity) * (1 - od.discount) + o.freight  as net_revenue
+    , od.discount                                        as product_discount_percentage
+    , (od.unit_price * od.quantity)                      as product_sales_revenue
+    , (od.unit_price * od.quantity) * (od.discount)      as product_discount_value
+    , (od.unit_price * od.quantity)                      as gross_revenue
+    , (od.unit_price * od.quantity) * (1 - od.discount)  as net_revenue
     , o.required_date
     , o.shipped_date
-    , case 
-        when o.shipped_date is not null 
-        then 'Yes' 
-        else 'No' 
-      end as shipped
-    , case 
-        when DATE_DIFF(o.shipped_date, o.required_date, DAY) > 0 
-        then 'Yes'
-        else 'No'
-      end as delayed
-    , case 
-        when DATE_DIFF(o.shipped_date, o.required_date, DAY) > 0 
-          then DATE_DIFF(o.shipped_date, o.required_date, DAY)
-        else NULL
-      end as delayed_days
-    , case
-        when o.shipped_date is not null 
-        then DATE_DIFF(o.shipped_date, o.order_date, DAY)
-        else NULL
-      end as shipping_time
+    , o.shipped_flag
+    , o.delayed_flag
+    , o.delayed_days
+    , o.shipping_time
     , o.ship_via
     , o.ship_name
     , o.ship_address
@@ -42,8 +32,41 @@ with fact_orders as (
     , o.ship_region
     , o.ship_postal_code
     , o.ship_country
-  from {{ ref('orders') }} as o
-  left join {{ ref('order_details') }} as od on o.order_id = od.order_id
-  left join {{ ref('products') }} as p on od.product_id = p.product_id
+  from {{ ref('stg_orders') }} as o
+  left join {{ ref('stg_order_details') }} as od on o.order_id = od.order_id
+)
+, freight_as_order_detail as (
+    select 
+        order_id
+      , customer_id
+      , employee_id
+      , order_date
+      , 78 as product_id
+      , freight as unit_price
+      , 1 as quantity
+      , 0 as product_discount_percentage
+      , 0 as product_sales_revenue
+      , 0 as product_discount_value
+      , freight as gross_revenue
+      , freight as net_revenue
+      , required_date
+      , shipped_date
+      , shipped_flag
+      , delayed_flag
+      , delayed_days
+      , shipping_time
+      , ship_via
+      , ship_name
+      , ship_address
+      , ship_city
+      , ship_region
+      , ship_postal_code
+      , ship_country
+    from {{ ref('stg_orders') }} as o
+)
+, fact_orders as (
+  select * from order_and_details
+  union all
+  select * from freight_as_order_detail
 )
 select * from fact_orders
--- penna_airflow_dbt/models/stage/northwind/stg_orders.sql
+++ penna_airflow_dbt/models/stage/northwind/stg_orders.sql
@@ -1,5 +1,40 @@
 with source as (
-  select * from {{ source('northwind', 'orders') }}
+  select 
+      order_id
+    , customer_id
+    , employee_id
+    , order_date
+    , freight
+    , required_date
+    , shipped_date
+    , case 
+        when shipped_date is not null 
+        then 'Yes' 
+        else 'No' 
+      end as shipped_flag
+    , case 
+        when DATE_DIFF(shipped_date, required_date, DAY) > 0 
+        then 'Yes'
+        else 'No'
+      end as delayed_flag
+    , case 
+        when DATE_DIFF(shipped_date, required_date, DAY) > 0 
+          then DATE_DIFF(shipped_date, required_date, DAY)
+        else NULL
+      end as delayed_days
+    , case
+        when shipped_date is not null 
+        then DATE_DIFF(shipped_date, order_date, DAY)
+        else NULL
+      end as shipping_time
+    , ship_via
+    , ship_name
+    , ship_address
+    , ship_city
+    , ship_region
+    , ship_postal_code
+    , ship_country
+  from {{ source('northwind', 'orders') }}
 )
 
 select * from source
