e38841ce7f64cb093be63ffeb8235fb42b181528
Pennacchi <rodrigopennacchi@gmail.com>
2025-07-24 19:34:31 -0300

    Create datamart and fix docs of each model area

--- penna_airflow_dbt/models/stage/erp_new_system/stg_new_employees.sql
+++ penna_airflow_dbt/models/stage/erp_new_system/stg_new_employees.sql
@@ -3,7 +3,7 @@ with source as (
       'erp_new_system||' || employee_id as employee_id
     , name
     , title
-    , boss_id
+    , 'erp_new_system||' || boss_id as boss_id
   from {{ source('erp_new_system', 'new_employees') }}
 ) 
 select * from source
