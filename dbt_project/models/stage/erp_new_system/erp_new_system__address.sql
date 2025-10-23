53787333065f4b2de5b97c98922bc7fb00cda617
Pennacchi <rodrigopennacchi@gmail.com>
2025-10-23 16:28:51 -0300

    Improve ci_cd

--- dbt_project/models/stage/erp_new_system/erp_new_system__address.sql
+++ dbt_project/models/stage/erp_new_system/erp_new_system__address.sql
@@ -7,6 +7,7 @@ with source as (
     , ship_region
     , cast(ship_postal_code as string) as ship_postal_code 
     , ship_country
+    , 1 as test_new_column_git_hub_action____please_remove_this_column
   from {{ source('erp_new_system', 'aws_s3__erp_new_system__new_address') }}
 ) 
 select * from source
