c2bad262cbe51dba6a3cf56e59ac4d031b0b0937
Pennacchi <rodrigopennacchi@gmail.com>
2025-10-23 19:13:38 -0300

    Adding partition to fact_orders

--- dbt_project/models/datamart/fact_orders.sql
+++ dbt_project/models/datamart/fact_orders.sql
@@ -1,3 +1,15 @@
+{{
+  config(
+    materialized='table',
+    partition_by={
+      'field': 'order_date', 
+      'data_type': 'datetime',
+      'granularity': 'day'
+    },
+    cluster_by=['product_id']
+  )
+}}
+
 with fact_orders as (
   select * from {{ ref('int_fact_orders') }}
 )
